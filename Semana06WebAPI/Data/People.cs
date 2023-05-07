a7c8ba982f6371c4eab782c72ded41476cdf0f71
Luis Chang <luis.chang@outlook.com>
2023-05-06 20:53:09 -0500

    Agregar archivos de proyecto.

---
+++ Semana06WebAPI/Data/People.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Semana06WebAPI.Data;
+
+public partial class People
+{
+    public int Id { get; set; }
+
+    public string? FullName { get; set; }
+
+    public string? Email { get; set; }
+
+    public bool? IsActive { get; set; }
+}
---
+++ Semana06WebAPI/Data/PeruContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Semana06WebAPI.Data;
+
+public partial class PeruContext : DbContext
+{
+    public PeruContext()
+    {
+    }
+
+    public PeruContext(DbContextOptions<PeruContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<People> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=WS1602493;Database=PERU;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<People>(entity =>
+        {
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(200);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
