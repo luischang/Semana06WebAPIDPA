3f72f9094ff89dc36ac3fe60e9473f37b00bf543
Luis Chang <luis.chang@outlook.com>
2023-05-06 21:11:29 -0500

    Se agrego la clase Product y su controlador con endpoints

--- Semana06WebAPI/Data/PeruContext.cs
+++ Semana06WebAPI/Data/PeruContext.cs
@@ -17,6 +17,8 @@ public partial class PeruContext : DbContext
 
     public virtual DbSet<People> People { get; set; }
 
+    public virtual DbSet<Product> Product { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=WS1602493;Database=PERU;Integrated Security=true;TrustServerCertificate=True");
@@ -27,6 +29,12 @@ public partial class PeruContext : DbContext
         {
             entity.Property(e => e.Email).HasMaxLength(100);
             entity.Property(e => e.FullName).HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Description).HasMaxLength(200);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
         });
 
         OnModelCreatingPartial(modelBuilder);
---
+++ Semana06WebAPI/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Semana06WebAPI.Data;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+}
